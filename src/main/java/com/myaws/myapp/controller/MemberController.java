e3f4cf6313c46ac2f5d589d6ef15411924feb23c
chan0116 <ktiun9630>
2024-10-31 17:37:00 +0900

    2024 10 31 SpringBasic FristClass

--- src/main/java/com/myaws/myapp/controller/MemberController.java
+++ src/main/java/com/myaws/myapp/controller/MemberController.java
@@ -1,16 +1,31 @@
 package com.myaws.myapp.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.myaws.myapp.service.Test;
+
 @Controller
+@RequestMapping(value = "/member/")
 public class MemberController {
 	
-	@RequestMapping(value = "/member/memberJoin.aws",method = RequestMethod.GET)
+	
+	private static final Logger logger = LoggerFactory.getLogger(MemberController.class);
+	
+	@Autowired
+	private Test tt;
+	
+	
+	@RequestMapping(value = "memberJoin.aws",method = RequestMethod.GET)
 	public String memberJoin() {
 		
+		logger.info("memberJoinµé¾î¿È");
 		
+		logger.info("tt°ªÀº? : "+tt.test());
 		
 		return "WEB-INF/member/memberJoin";
 	}
@@ -18,7 +33,7 @@ public class MemberController {
 	
 	
 	
-	@RequestMapping(value = "/member/memberLogin.aws",method = RequestMethod.GET)
+	@RequestMapping(value = "memberLogin.aws",method = RequestMethod.GET)
 	public String memberLogin() {
 		
 		
---
+++ src/main/java/com/myaws/myapp/service/Test.java
@@ -0,0 +1,21 @@
+package com.myaws.myapp.service;
+
+public class Test {
+	
+	
+	private String name = "스프링공부";
+	
+	
+	public String test() {
+		
+		String humanName = "홍길동";
+		
+		
+		String value = humanName + name; 
+		
+		return value;
+	}
+	
+	
+
+}
