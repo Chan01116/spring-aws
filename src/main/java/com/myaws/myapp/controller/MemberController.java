f5a09ce00dc6072911856fb76d60d6eac1056e87
chan0116 <ktiun9630>
2024-11-04 16:44:41 +0900

    2024 11 04 아이디 및 비밀번호 일치확인

--- src/main/java/com/myaws/myapp/controller/MemberController.java
+++ src/main/java/com/myaws/myapp/controller/MemberController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.myaws.myapp.domain.MemberVo;
 import com.myaws.myapp.service.MemberService;
@@ -85,25 +86,39 @@ public class MemberController {
 	}
 	
 	@RequestMapping(value="memberLoginAction.aws",method=RequestMethod.POST)
-	public String memberLoginAction(@RequestParam("memberid") String memberid, @RequestParam("memberpwd") String memberpwd ) {		
+	public String memberLoginAction(
+			@RequestParam("memberid") String memberid, 
+			@RequestParam("memberpwd") String memberpwd,
+			RedirectAttributes rttr
+			) {		
 		
 		MemberVo mv = memberService.memberLoginCheck(memberid);
 		//저장된 비밀번호를 가져온다
 		
 		String path = "";
-		if (mv != null) {  //객체값이 없으면
+		if (mv != null) {  //객체값이 있으면
 			String reservedPwd = mv.getMemberpwd(); 
 		
 			if(bCryptPasswordEncoder.matches(memberpwd, reservedPwd)) {
-				System.out.println("비밀번호 일치");
-				
+				//System.out.println("비밀번호 일치");
+				rttr.addAttribute("midx", mv.getMidx());
+				rttr.addAttribute("memberId", mv.getMemberid());
+				rttr.addAttribute("memberName", mv.getMembername());
+								
 				path ="redirect:/";
 			}else {
 				
+				//rttr.addAttribute("midx", "");
+				//rttr.addAttribute("memberId", "");
+				//rttr.addAttribute("memberName", "");
+				rttr.addFlashAttribute("msg", "아이디/비밀번호를 확인해주세요.");
 				path = "redirect:/member/memberLogin.aws";
 			}		
 		}else {
-			
+			//rttr.addAttribute("midx", "");
+			//rttr.addAttribute("memberId", "");
+			//rttr.addAttribute("memberName", "");    //위험해서 안씀
+			rttr.addFlashAttribute("msg", "해당하는 아이디가 없습니다.");  //플래쉬는 한번만
 			path = "redirect:/member/memberLogin.aws";
 		}		
 		//회원정보를  세션에 담는다
--- src/main/webapp/WEB-INF/member/memberLogin.jsp
+++ src/main/webapp/WEB-INF/member/memberLogin.jsp
@@ -1,5 +1,11 @@
-<!DOCTYPE HTML>
 <%@ page language="java" contentType="text/html; charset=UTF-8"     pageEncoding="UTF-8"%>
+<%
+	String msg = "";
+	if(request.getAttribute("msg")!= null){ 
+	msg = (String)request.getAttribute("msg");
+	}
+%>
+<!DOCTYPE HTML>
 <HTML>
  <HEAD>
   <TITLE> memberJoin.html </TITLE>
@@ -46,6 +52,11 @@
   </style>
   
   <script>
+  <%
+  if(msg != ""){
+  out.println("alert('"+msg+"')");
+  }
+  %>
   //아이디 비밀번호 유효성 검사
   function check(){
 	  //이름으로 객체찾기
